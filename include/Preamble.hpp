564b7526b8e790f28fc7f275e9f834782b6ea78e
Claude Schmit <claudejpschmit@gmailcom>
2013-11-09 18:29:30 +0000

    Included all files and set stage

---
+++ include/Preamble.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <assert.h>
+#include <time.h>
+
+using namespace std;
---
+++ src/Main.cpp
@@ -0,0 +1,7 @@
+#include "Preamble.hpp"
+#include "helpers.hpp"
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
